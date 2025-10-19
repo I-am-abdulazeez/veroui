99e911e5265383ed1aa78c183e544bbfbd70945f
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-19 17:11:44 +0100

    chore(core): remove core package and its related files

--- packages/core/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export type Size = 'sm' | 'md' | 'lg' | 'xl'
-export type Color = 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger'
-export type Variant = 'solid' | 'bordered' | 'light' | 'flat' | 'faded' | 'shadow' | 'ghost'
-export type Radius = 'none' | 'sm' | 'md' | 'lg' | 'full'
-
-export interface BaseComponentProps {
-  class?: string
-  style?: string | Record<string, string>
-}
