019d7539cb9a5aebf848b9eaa3a0b473bfb41742
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-21 04:25:24 +0100

    feat(composables): add composable exports for label placement functionality

---
+++ packages/core/system/src/composables/index.ts
@@ -0,0 +1,8 @@
+export {
+  useLabelPlacement,
+  useInputLabelPlacement,
+  type LabelPlacement,
+  type InputLabelPlacement,
+  type UseLabelPlacementProps,
+  type UseInputLabelPlacementProps,
+} from './use-label-placement'
