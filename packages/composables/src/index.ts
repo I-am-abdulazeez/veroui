8c9279e0e33557336067c507f6b1b9cbe9ab376c
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-21 19:44:16 +0100

    feat(composables): export theme composables

--- packages/composables/src/index.ts
+++ packages/composables/src/index.ts
@@ -1,2 +1,12 @@
+// state management
 export { useControllableState, type UseControllableStateProps } from './use-controllable-state'
 export { useDisclosure, type UseDisclosureProps, type UseDisclosureReturn } from './use-disclosure'
+
+// Theme
+export {
+  useTheme,
+  ThemeProps,
+  type Theme,
+  type CustomTheme,
+  type UseThemeReturn
+} from './use-theme'
