d6d56de2c68b5d6314ccfd8de35e3c33b1a3b745
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-21 19:49:07 +0100

    feat(tsup): add initial tsup configuration for building the vue-utils

---
+++ packages/utilities/vue-utils/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  clean: true,
+  target: "es2019",
+  format: ["cjs", "esm"],
+  dts: true,
+  external: ["vue"],
+})
