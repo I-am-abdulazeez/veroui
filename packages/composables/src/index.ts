9e3ad4ee4e3c3f127118b113dcfda0253109bb57
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-22 14:59:50 +0100

    chore(composables): bring in observers

--- packages/composables/src/index.ts
+++ packages/composables/src/index.ts
@@ -1,3 +1,10 @@
+// Observers
+export {
+  useIntersectionObserver,
+  type UseIntersectionObserverOptions,
+  type IntersectionReturn,
+} from './use-intersection-observer'
+
 // State management
 export { useControllableState, type UseControllableStateProps } from './use-controllable-state'
 export { useDisclosure, type UseDisclosureProps, type UseDisclosureReturn } from './use-disclosure'
