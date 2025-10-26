25af7000af30e9905cfdbf7dfeec6f19208e2035
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-26 07:20:30 +0100

    feat(components): add ButtonGroup component to handle structure and style in the vue way

---
+++ packages/components/button/src/button-group.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { provideButtonGroupContext } from "./button-group-context";
+import { useButtonGroup } from "./use-button-group";
+import type { ButtonGroupProps } from "./types";
+
+const props = withDefaults(defineProps</* @vue-ignore */ ButtonGroupProps>(), {
+  as: "div",
+  size: "md",
+  color: "default",
+  variant: "solid",
+  isDisabled: false,
+  isIconOnly: false,
+  disableRipple: false,
+  disableAnimation: false,
+  fullWidth: false,
+});
+
+const { Component, context, buttonGroupProps } = useButtonGroup(props);
+
+// Provide context to child buttons
+provideButtonGroupContext(context.value);
+</script>
+
+<template>
+  <component :is="Component" v-bind="buttonGroupProps">
+    <slot />
+  </component>
+</template>
