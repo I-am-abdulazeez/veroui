05541e688ea43f28902b782c102c7a07f559f3e6
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-29 15:28:40 +0100

    chore(histoire): add css styles that bring in tailwind

--- packages/storybook/histoire.setup.ts
+++ packages/storybook/histoire.setup.ts
@@ -1,5 +1,5 @@
 import { defineSetupVue3 } from '@histoire/plugin-vue'
-// import './src/styles/index.css'
+import './src/styles/index.css'
 
 export const setupVue3 = defineSetupVue3(({ app, story, variant }) => {
   // Add global configurations here
