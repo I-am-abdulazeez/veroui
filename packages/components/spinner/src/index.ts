1485accff0770cad15c48d870e9b7a778b2c34c5
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-30 14:09:38 +0100

    feat(component): export under index

---
+++ packages/components/spinner/src/index.ts
@@ -0,0 +1,8 @@
+// Export types
+export type { SpinnerProps, SpinnerVariant, SpinnerSize, SpinnerColor, SpinnerSlots } from './types'
+
+// Export composable
+export { useSpinner, type UseSpinnerProps, type UseSpinnerReturn } from './use-spinner'
+
+// Export component
+export { default as Spinner } from './spinner.vue'
