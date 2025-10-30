7b382e5b1b70b15ffa7b4d7675e7c9f1b781eb77
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-30 18:49:10 +0100

    feat(component): export in index for modularity

---
+++ packages/components/ripple/src/index.ts
@@ -0,0 +1,10 @@
+import Ripple from './ripple.vue'
+
+// export types
+export type { RippleType } from './types'
+
+// export hooks
+export { useRipple } from './use-ripple'
+
+// export component
+export { Ripple }
